154c64a454394b4db056eaa3cf2a80d2e4612f8f
Benjamin Runnels <kraven@kraven.org>
2016-04-09 15:51:12 -0500

    windows and visual studio fixes and updates

--- include/c_types.h
+++ include/c_types.h
@@ -12,20 +12,19 @@
 #ifndef _C_TYPES_H_
 #define _C_TYPES_H_
 
-#include <stdint.h>
 #include <stdbool.h>
 
-//typedef unsigned char       uint8_t;
+typedef unsigned char       uint8_t;
 typedef signed char         sint8_t;
-//typedef signed char         int8_t;
-//typedef unsigned short      uint16_t;
+typedef signed char         int8_t;
+typedef unsigned short      uint16_t;
 typedef signed short        sint16_t;
-//typedef signed short        int16_t;
-//typedef unsigned long       uint32_t;
-typedef signed long         sint32_t;
-//typedef signed long         int32_t;
+typedef signed short        int16_t;
+typedef unsigned int        uint32_t;
+typedef signed int          sint32_t;
+typedef signed int          int32_t;
 typedef signed long long    sint64_t;
-//typedef unsigned long long  uint64_t;
+typedef unsigned long long  uint64_t;
 typedef unsigned long long  u_int64_t;
 typedef float               real32_t;
 typedef double              real64_t;
