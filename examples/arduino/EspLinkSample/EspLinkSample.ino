364612c718f3e455ce1167b8681080ab79a4e3a4
Karai Csaba <cskarai@freemail.hu>
2016-05-14 19:37:09 +0200

    LED on / off and blinking

--- examples/arduino/EspLinkSample/EspLinkSample.ino
+++ examples/arduino/EspLinkSample/EspLinkSample.ino
@@ -1,6 +1,87 @@
 
 #include "WebServer.h"
+
+#define LED_PIN  13
+
+int8_t blinking = 0;
+int8_t frequency = 10;
+uint16_t elapse = 100;
+uint32_t next_ts = 0;
+
+void ledHtmlCallback(WebServerCommand command, char * data, int dataLen);
+
+const char ledURL[] PROGMEM = "/LED.html.json";
+
+const WebMethod PROGMEM methods[] = {
+  { ledURL, ledHtmlCallback },
+  { NULL, NULL },
+};
+
+WebServer webServer(Serial, methods);
+
+void setup()
+{
+  Serial.begin(57600);
+  webServer.init();
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, false);
+}
+
+void loop()
+{
+  webServer.loop();
+  
+  if( blinking )
+  {
+    if( next_ts <= millis() )
+    {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      next_ts += elapse;
+    }
+  }
+}
+
+
+void ledHtmlCallback(WebServerCommand command, char * data, int dataLen)
+{
+  switch(command)
+  {
+    case BUTTON_PRESS:
+      if( strcmp_P(data, PSTR("btn_on") ) == 0 )
+      {
+        blinking = 0;
+        digitalWrite(LED_PIN, true);
+      } else if( strcmp_P(data, PSTR("btn_off") ) == 0 )
+      {
+        blinking = 0;
+        digitalWrite(LED_PIN, false);
+      } else if( strcmp_P(data, PSTR("btn_blink") ) == 0 )
+      {
+        blinking = 1;
+        next_ts = millis() + elapse;
+      }
+      break;
+    case LOAD:
+      webServer.setArgNum(1);
+    case REFRESH:
+      if( command == REFRESH )
+        webServer.setArgNum(1);
+
+      if( blinking )
+        webServer.setArgStringP("text", PSTR("LED is blinking"));
+      else
+        webServer.setArgStringP("text", digitalRead(LED_PIN) ? PSTR("LED is turned on") : PSTR("LED is turned off"));
+      break;
+    default:
+      break;
+  }
+}
+
+
+/*
 #include "EspLink.h"
+
 
 void packetReceived(CmdRequest *req);
 
@@ -97,4 +178,4 @@ void setup() {
 void loop() {
   espLink.readLoop();
 }
-
+*/
--- examples/arduino/EspLinkSample/WebServer.h
+++ examples/arduino/EspLinkSample/WebServer.h
@@ -1,11 +1,78 @@
 #ifndef WEB_SERVER_H
 #define WEB_SERVER_H
 
-typedef enum {
-  LOAD=0,
+#include "EspLink.h"
+
+typedef enum
+{
+  BUTTON_PRESS,
+  SET_FIELD,
   REFRESH,
-  BUTTON,
-  SUBMIT,
+  LOAD,
+} WebServerCommand;
+
+typedef void (*WebServerCallback)(WebServerCommand command, char * data, int dataLen);
+
+typedef struct
+{
+  const char *  PROGMEM  url;
+  WebServerCallback      callback;
+} WebMethod;
+
+
+typedef enum {
+  WS_LOAD=0,
+  WS_REFRESH,
+  WS_BUTTON,
+  WS_SUBMIT,
 } RequestReason;
 
+typedef enum
+{
+  WEB_STRING=0,
+  WEB_NULL,
+  WEB_INTEGER,
+  WEB_BOOLEAN,
+  WEB_FLOAT,
+  WEB_JSON
+} WebValueType;
+
+class WebServer
+{
+  friend void webServerCallback(CmdRequest *req);
+  
+  private:
+    const WebMethod * PROGMEM methods;
+    Stream                   &stream;
+    static WebServer *        instance;
+
+    void                      invokeMethod(RequestReason reason, WebMethod * method, CmdRequest *req);
+    void                      handleRequest(CmdRequest *req);
+
+    uint8_t                   remote_ip[4];
+    uint16_t                  remote_port;
+
+    int16_t                   args_to_send;
+    
+  protected:
+    EspLink espLink;
+    
+  public:
+    WebServer(Stream &stream, const WebMethod * PROGMEM methods);
+
+    void init();
+    void loop();
+
+    void registerCallback();
+
+    static WebServer * getInstance() { return instance; }
+    uint8_t *          getRemoteIp() { return remote_ip; }
+    uint16_t           getRemotePort() { return remote_port; }
+
+    void               setArgNum(uint8_t num);
+    void               setArgString(const char * name, const char * value);
+    void               setArgStringP(const char * name, const char * value);
+};
+
 #endif /* WEB_SERVER_H */
+
