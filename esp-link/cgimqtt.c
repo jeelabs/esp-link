1e87efec8cda2e61f2d97db70744319f968677d6
Thorsten von Eicken <tve@rightscale.com>
2016-06-18 22:54:07 -0700

    Revert "Fix some type casts per #129"
    This reverts commit 52843c6316b28b4c76b4875a98e2725de0c446c9.

--- esp-link/cgimqtt.c
+++ esp-link/cgimqtt.c
@@ -95,11 +95,11 @@ int ICACHE_FLASH_ATTR cgiMqttSet(HttpdConnData *connData) {
 
   if (mqtt_server < 0) return HTTPD_CGI_DONE;
   mqtt_server |= getBoolArg(connData, "mqtt-clean-session",
-      (bool *)&flashConfig.mqtt_clean_session);
+      &flashConfig.mqtt_clean_session);
 
   if (mqtt_server < 0) return HTTPD_CGI_DONE;
   int8_t mqtt_en_chg = getBoolArg(connData, "mqtt-enable",
-      (bool *)&flashConfig.mqtt_enable);
+      &flashConfig.mqtt_enable);
 
   char buff[16];
 
@@ -134,7 +134,7 @@ int ICACHE_FLASH_ATTR cgiMqttSet(HttpdConnData *connData) {
     mqtt_client_init();
 
   // if just enable changed we just need to bounce the client
-  }
+  } 
   else if (mqtt_en_chg > 0) {
     DBG("MQTT server enable=%d changed\n", flashConfig.mqtt_enable);
     if (flashConfig.mqtt_enable && strlen(flashConfig.mqtt_host) > 0)
@@ -145,16 +145,16 @@ int ICACHE_FLASH_ATTR cgiMqttSet(HttpdConnData *connData) {
 
   // no action required if mqtt status settings change, they just get picked up at the
   // next status tick
-  if (getBoolArg(connData, "mqtt-status-enable", (bool *)&flashConfig.mqtt_status_enable) < 0)
+  if (getBoolArg(connData, "mqtt-status-enable", &flashConfig.mqtt_status_enable) < 0)
     return HTTPD_CGI_DONE;
   if (getStringArg(connData, "mqtt-status-topic",
         flashConfig.mqtt_status_topic, sizeof(flashConfig.mqtt_status_topic)) < 0)
     return HTTPD_CGI_DONE;
 
   // if SLIP-enable is toggled it gets picked-up immediately by the parser
-  int slip_update = getBoolArg(connData, "slip-enable", (bool *)&flashConfig.slip_enable);
+  int slip_update = getBoolArg(connData, "slip-enable", &flashConfig.slip_enable);
   if (slip_update < 0) return HTTPD_CGI_DONE;
-  if (slip_update > 0)
+  if (slip_update > 0) 
     DBG("SLIP-enable changed: %d\n", flashConfig.slip_enable);
 
   DBG("Saving config\n");
