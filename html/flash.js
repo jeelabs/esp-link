f98b66dcb4285743f2f0d079d6eabe978a4cf399
nemik <nemik@nemik.net>
2016-07-22 13:18:24 -0500

    nicer formatting

--- html/flash.js
+++ html/flash.js
@@ -2,37 +2,37 @@
 
 function flashFirmware(e) {
   e.preventDefault();
-	var fw_data = document.getElementById('fw-file').files[0];
-			
-	$("#fw-form").setAttribute("hidden", "");
-	$("#fw-spinner").removeAttribute("hidden");
+  var fw_data = document.getElementById('fw-file').files[0];
+      
+  $("#fw-form").setAttribute("hidden", "");
+  $("#fw-spinner").removeAttribute("hidden");
   showNotification("Firmware is being updated ...");
 
-	nanoajax.ajax({url: '/flash/upload', method: 'POST', body: fw_data}, function (code, responseText, request) {
-		if(""+code == "200")
-		{
-			ajaxReq("GET", "/flash/reboot", function (resp) {
-    		showNotification("Firmware has been successfully updated!");
-				setTimeout(function(){ window.location.reload()}, 4000);
+  nanoajax.ajax({url: '/flash/upload', method: 'POST', body: fw_data}, function (code, responseText, request) {
+    if(""+code == "200")
+    {
+      ajaxReq("GET", "/flash/reboot", function (resp) {
+        showNotification("Firmware has been successfully updated!");
+        setTimeout(function(){ window.location.reload()}, 4000);
 
-				$("#fw-spinner").setAttribute("hidden", "");
-				$("#fw-form").removeAttribute("hidden");
-			});
-		}
-	})
+        $("#fw-spinner").setAttribute("hidden", "");
+        $("#fw-form").removeAttribute("hidden");
+      });
+    }
+  })
 }
 
 function fetchFlash() {
   ajaxReq("GET", "/flash/next", function (resp) {
-		$("#fw-slot").innerHTML = resp;
-  	$("#fw-spinner").setAttribute("hidden", "");
-  	$("#fw-form").removeAttribute("hidden");
+    $("#fw-slot").innerHTML = resp;
+    $("#fw-spinner").setAttribute("hidden", "");
+    $("#fw-form").removeAttribute("hidden");
   });
-	ajaxJson("GET", "/menu", function(data) {
+  ajaxJson("GET", "/menu", function(data) {
       var v = $("#current-fw");
       if (v != null) { v.innerHTML = data.version; }
     }
-	);
+  );
 }
 
 function setMqtt(name, v) {
