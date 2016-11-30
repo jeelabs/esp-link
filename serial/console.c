b3d8072bae3421f11355e19376aa48cc336c4d5e
hetii <ghetman@gmail.com>
2016-11-30 17:29:07 +0100

    Add missing baudrate settings. (#216)

--- serial/console.c
+++ serial/console.c
@@ -67,7 +67,7 @@ ajaxConsoleBaud(HttpdConnData *connData) {
   len = httpdFindArg(connData->getArgs, "rate", buff, sizeof(buff));
   if (len > 0) {
     int rate = atoi(buff);
-    if (rate >= 9600 && rate <= 1000000) {
+    if (rate >= 300 && rate <= 1000000) {
       uart0_baud(rate);
       flashConfig.baud_rate = rate;
       status = configSave() ? 200 : 400;
